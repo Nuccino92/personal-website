991e7fd1462cd3895fb370814e113bf2fa4ff65c
Nuccino92 <anthnucci@hotmail.com>
2022-04-03 20:35:02 -0400

    header component

---
+++ src/components/header/Header.js
@@ -0,0 +1,15 @@
+import "./Header.scss";
+import { useEffect, useState } from "react";
+
+const Header = () => {
+  return (
+    <div className="Header">
+      <h1>Anthony Nucci</h1>
+
+      <div className="typewriter">
+        <p>Full stack developer</p>
+      </div>
+    </div>
+  );
+};
+export default Header;
