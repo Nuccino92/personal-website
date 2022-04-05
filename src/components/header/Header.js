b7b5ee7aba1d52c52a4203d14b8e675bfd735b35
Nuccino92 <anthnucci@hotmail.com>
2022-04-04 20:57:26 -0400

    finished header

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,14 +1,27 @@
+import { useRef } from "react";
 import "./Header.scss";
-import { useEffect, useState } from "react";
 
 const Header = () => {
+  const dragRef = useRef(null);
+
+  const handleDrag = (e) => {
+    const div = dragRef.current;
+    div.className = "dragged";
+  };
+
   return (
     <div className="Header">
-      <h1>Anthony Nucci</h1>
+      <header>
+        <span>AN</span>
+        <h1 ref={dragRef} draggable="true" onDragEnd={(e) => handleDrag(e)}>
+          thony Nucci
+        </h1>
+      </header>
 
       <div className="typewriter">
         <p>Full stack developer</p>
       </div>
+      <button>CONTACT ME</button>
     </div>
   );
 };
