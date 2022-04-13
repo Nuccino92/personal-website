c61dfaf51827403086343ef7233277963e7c49b1
Nuccino92 <anthnucci@hotmail.com>
2022-04-12 21:15:10 -0400

    added a mobile nav

--- src/components/nav/Nav.js
+++ src/components/nav/Nav.js
@@ -1,13 +1,44 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import "./Nav.scss";
 import { FiSun } from "react-icons/fi";
 import { BsFillMoonStarsFill } from "react-icons/bs";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { GrClose } from "react-icons/gr";
 
-const Nav = ({ lightTheme, setLightTheme }) => {
+const Nav = ({
+  lightTheme,
+  setLightTheme,
+  refHeader,
+  refAbout,
+  refExperience,
+  refContact,
+}) => {
   const navRef = useRef(null);
+
+  const [smallNav, setSmallNav] = useState(false);
 
   const handleClick = () => {
     setLightTheme((prev) => !prev);
+  };
+
+  const handleHeaderScroll = () => {
+    refHeader.current.scrollIntoView({ behavior: "smooth" });
+    setSmallNav(false);
+  };
+
+  const handleAboutScroll = () => {
+    refAbout.current.scrollIntoView({ behavior: "smooth" });
+    setSmallNav(false);
+  };
+
+  const handleExperienceScroll = () => {
+    refExperience.current.scrollIntoView({ behavior: "smooth" });
+    setSmallNav(false);
+  };
+
+  const handleContactScroll = () => {
+    refContact.current.scrollIntoView({ behavior: "smooth" });
+    setSmallNav(false);
   };
 
   useEffect(() => {
@@ -18,33 +49,77 @@ const Nav = ({ lightTheme, setLightTheme }) => {
   }, []);
 
   return (
-    <div
-      className="Nav"
-      style={
-        lightTheme
-          ? { backgroundColor: "white" }
-          : { backgroundColor: "rgb(24, 24, 24)" }
-      }
-      ref={navRef}
-    >
-      {/* logo */}
-      <h1>AN</h1>
-      <ul>
-        <li>About</li>
-        <li>Experience</li>
-        <li>Contact</li>
+    <>
+      <div
+        className="Nav"
+        style={
+          lightTheme
+            ? { backgroundColor: "white" }
+            : { backgroundColor: "rgb(24, 24, 24)" }
+        }
+        ref={navRef}
+      >
+        {/* logo */}
+        <h1 onClick={handleHeaderScroll}>AN</h1>
+        <ul>
+          <li onClick={handleAboutScroll}>About</li>
+          <li onClick={handleExperienceScroll}>Experience</li>
+          <li onClick={handleContactScroll}>Contact</li>
 
-        {lightTheme ? (
-          <BsFillMoonStarsFill
-            className="change-theme moon"
-            size={21}
-            onClick={handleClick}
+          {lightTheme ? (
+            <BsFillMoonStarsFill
+              className="change-theme moon"
+              size={21}
+              onClick={handleClick}
+            />
+          ) : (
+            <FiSun
+              className="change-theme sun"
+              size={21}
+              onClick={handleClick}
+            />
+          )}
+          <GiHamburgerMenu
+            className="hamburger"
+            size={33}
+            onClick={() => setSmallNav(true)}
           />
-        ) : (
-          <FiSun className="change-theme sun" size={21} onClick={handleClick} />
-        )}
-      </ul>
-    </div>
+        </ul>
+      </div>
+      {smallNav && (
+        <div className="nav-modal">
+          <div className="side-nav">
+            <ul>
+              <GrClose
+                size={33}
+                color={"white"}
+                onClick={() => setSmallNav(false)}
+                className="close"
+              />
+              <li onClick={handleAboutScroll}>About</li>
+              <li onClick={handleExperienceScroll}>Experience</li>
+              <li onClick={handleContactScroll}>Contact</li>
+
+              {lightTheme ? (
+                <BsFillMoonStarsFill
+                  className="change-theme moon"
+                  size={21}
+                  onClick={handleClick}
+                  style={{ display: "initial", color: "white" }}
+                />
+              ) : (
+                <FiSun
+                  style={{ display: "initial", color: "white" }}
+                  className="change-theme sun"
+                  size={21}
+                  onClick={handleClick}
+                />
+              )}
+            </ul>
+          </div>
+        </div>
+      )}
+    </>
   );
 };
 
