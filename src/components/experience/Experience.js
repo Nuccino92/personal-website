bbed65bbba7afd79c6de965e4bfca73c389d8872
Nuccino92 <anthnucci@hotmail.com>
2022-04-17 19:59:14 -0400

    adjusted grammar

--- src/components/experience/Experience.js
+++ src/components/experience/Experience.js
@@ -5,7 +5,7 @@ import Projects from "./project/projects.json";
 const Experience = ({ refExperience }) => {
   return (
     <div className="Experience" ref={refExperience}>
-      <h1> Some of the things i've built </h1>
+      <h1> Some of the things I've built </h1>
       <main>
         {Projects.map((project, index) => {
           return <Project project={project} key={index} index={index} />;
